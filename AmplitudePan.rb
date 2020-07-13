227bcaa9843e7e7f4b5897db0df43175f1bf1a49
abbabon <abbabon@gmail.com>
2020-07-14 00:06:31 +0300

    Tuesday, July 14, 2020.

---
+++ AmplitudePan.rb
@@ -0,0 +1,15 @@
+# should learn how to loop this
+play 50, amp: 0.1, pan: -1
+sleep 0.25
+play 55, amp: 0.2, pan: 1
+sleep 0.25
+play 57, amp: 0.4, pan: -1
+sleep 0.25
+play 62, amp: 1, pan: 0
+sleep 0.25
+play 57, amp: 0.4, pan: 1
+sleep 0.25
+play 55, amp: 0.2, pan: -1
+sleep 0.25
+play 50, amp: 0.1, pan: 1
+sleep 0.25
